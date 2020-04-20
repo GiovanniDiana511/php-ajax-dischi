5029041a43af0e6ec22897c1e1e5720c5179a825
Giovanni <giovanni.diana511@gmail.com>
2020-04-20 19:09:10 +0200

    Da completare

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="dist/css/app.css">
+        <title>SPAJaxfy</title>
+    </head>
+    <body>
+        <header>
+            <div class="container">
+                <div id="logo">
+                    <a href="#">
+                        <img src="https://cdn.iconscout.com/icon/free/png-256/spotify-33-675787.png" alt="logo">
+                    </a>
+                </div>
+            </div>
+        </header>
+        <main>
+            <div class="container">
+                <div class="container-inner">
+                    <div class="card">
+                        <div class="image">
+                            <img src="" alt="">
+                        </div>
+                        <div class="text">
+                            <h2>Titolo</h2>
+                            <h4>Nome Artista</h4>
+                            <p>Anno</p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="image">
+                            <img src="" alt="">
+                        </div>
+                        <div class="text">
+                            <h2>Titolo</h2>
+                            <h4>Nome Artista</h4>
+                            <p>Anno</p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="image">
+                            <img src="" alt="">
+                        </div>
+                        <div class="text">
+                            <h2>Titolo</h2>
+                            <h4>Nome Artista</h4>
+                            <p>Anno</p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="image">
+                            <img src="" alt="">
+                        </div>
+                        <div class="text">
+                            <h2>Titolo</h2>
+                            <h4>Nome Artista</h4>
+                            <p>Anno</p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="image">
+                            <img src="" alt="">
+                        </div>
+                        <div class="text">
+                            <h2>Titolo</h2>
+                            <h4>Nome Artista</h4>
+                            <p>Anno</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <footer>
+            <div class="container">
+
+            </div>
+        </footer>
+    </body>
+</html>
